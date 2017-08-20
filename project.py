442e7fad5ef7c8506ca83ce8a1dd4e3323b70f9b
lbuthman <lbuthman@gmail.com>
2017-08-20 15:06:49 -0500

    use templates for rendering views

--- project.py
+++ project.py
@@ -6,36 +6,37 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 @app.route('/')
-def home():
-    return "This will display the landing page."
+@app.route('/index.html')
+def index():
+    return render_template("index.html")
 
 @app.route('/login')
 def login():
-    return "Provide the user a login screen"
+    return render_template("login.html")
 
 @app.route('/<goal>')
 def view_goal(goal):
-    return "Returns all workouts for a certain goal."
+    return render_template("view-goal.html")
 
 @app.route('/<goal>/<workout>')
 def view_workout(goal, workout):
-    return "Retuns info for workout"
+    return render_template("view-workout.html")
 
 @app.route('/how-it-works')
 def how_it_works():
-    return "Provides info on how to use/navigate the site."
+    return render_template("how-it-works.html")
 
 @app.route('/<goal>/new')
 def new_workout(goal):
-    return "Let logged in user create a new workout"
+    return render_template("new-workout.html")
 
 @app.route('/<goal>/<workout>/edit')
 def edit_workout(goal, workout):
-    return "Provides a way to edit a workout"
+    return render_template("edit-workout.html")
 
 @app.route('/<goal>/<workout>/delete')
 def delete_workout(goal, workout):
-    return "Provide a way to delete a workout"
+    return render_template("delete-workout.html")
 
 if __name__ == "__main__":
     app.secret_key = "supersupersecretkey"
