a36907e7c167b1622deeca00e5f94b4459ada73a
lbuthman <lbuthman@gmail.com>
2017-08-20 15:09:26 -0500

    provide proper routing if address ends in /

--- project.py
+++ project.py
@@ -7,34 +7,42 @@ from sqlalchemy.orm import sessionmaker
 
 @app.route('/')
 @app.route('/index.html')
+@app.route('/index.html/')
 def index():
     return render_template("index.html")
 
 @app.route('/login')
+@app.route('/login/')
 def login():
     return render_template("login.html")
 
 @app.route('/<goal>')
+@app.route('/<goal>/')
 def view_goal(goal):
     return render_template("view-goal.html")
 
 @app.route('/<goal>/<workout>')
+@app.route('/<goal>/<workout>/')
 def view_workout(goal, workout):
     return render_template("view-workout.html")
 
 @app.route('/how-it-works')
+@app.route('/how-it-works/')
 def how_it_works():
     return render_template("how-it-works.html")
 
 @app.route('/<goal>/new')
+@app.route('/<goal>/new/')
 def new_workout(goal):
     return render_template("new-workout.html")
 
 @app.route('/<goal>/<workout>/edit')
+@app.route('/<goal>/<workout>/edit/')
 def edit_workout(goal, workout):
     return render_template("edit-workout.html")
 
 @app.route('/<goal>/<workout>/delete')
+@app.route('/<goal>/<workout>/delete/')
 def delete_workout(goal, workout):
     return render_template("delete-workout.html")
 
