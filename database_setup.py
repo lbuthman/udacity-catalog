ae0510781a0351c451c201a081fafe844537555b
lbuthman <lbuthman@gmail.com>
2017-08-21 18:17:17 -0500

    Add first setup of database in class

---
+++ database_setup.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+class Exercise(Base):
+    __tablename__ = 'exercise'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    description = Column(String(250))
+    url = Column(String(250), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'url': self.url,
+        }
+
+engine = create_engine('sqlite:///exercisecatalog.db')
+
+Base.metadata.create_all(engine)
