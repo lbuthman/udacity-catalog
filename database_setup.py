b5fa50aa8c8dfb33f5221c04c33b5cb57711a6c3
lbuthman <lbuthman@gmail.com>
2017-08-22 12:07:51 -0500

    add User class to database

--- database_setup.py
+++ database_setup.py
@@ -5,6 +5,23 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture,
+        }
+
 class Category(Base):
     __tablename__ = 'category'
 
