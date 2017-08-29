53a1ef798363862aa3c1e8ff636676280f914474
lbuthman <lbuthman@gmail.com>
2017-08-28 19:32:54 -0500

    first pass at Facebook Sign In

--- project.py
+++ project.py
@@ -153,6 +153,64 @@ def gdisconnect():
         flash("Failed to revoke token for given user", "danger")
         return redirect(url_for("index"))
 
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Obtain authorization code
+    access_token = request.data
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = """https://graph.facebook.com/oauth/access_token?grant_typ=fb_exchange_token
+        &client_id=%s&client_secret=%s&fb_exchange_token=%s""" % (app_id,
+        app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    userinfo_url = "https://graph.facebook.com/v2.2/me"
+    token = h.result.split("&")[0]
+
+    url = "https://graph.facebook.com/v2.8/me?%s&fields=name,id,email" % token
+    h = httplib2.Http()
+    result = request(url, 'GET')[1]
+
+    data = json.loads(result)
+
+    login_session['username'] = data['name']
+    login_session['email'] = data['email']
+    login_session['facebook_id'] = data['id']
+
+    user_id = get_user_id(session['email'])
+    if not user_id:
+        user_id = create_user(session)
+    session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += session['username']
+    output += '!</h1>'
+    flash("You are in! Feel free to add, edit or delete exercises.".format(
+        session['username']), "success")
+    return output
+
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    url = "https://graph.facebook.com/%s/permissions" % facebook_id
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    del login_session['username']
+    del login_session['email']
+    del login_session['user_id']
+    del login_session['facebook_id']
+    flash("You have been Successfully disconnected", "success")
+    return redirect(url_for("index"))
+
 @app.route('/categories/JSON/')
 def all_categoriesJSON():
     categories = db_session.query(Category).all()
@@ -258,7 +316,7 @@ def edit_exercise(category, exercise):
     else:
         return render_template("edit-exercise.html", category=category,
             categories=categories, exercise=editedExercise)
-#
+
 @app.route('/<category>/<exercise>/delete/', methods=['GET', 'POST'])
 def delete_exercise(category, exercise):
     if 'username' not in session:
